7c2dd3af4aa4533927b413cf548f0bea768aac80
siddharthasiddu11 <siddullll6@gmail.com>
2025-10-07 10:10:39 +0530

    Update Card.tsx

--- Second-Brain-Frontend/src/components/Card.tsx
+++ Second-Brain-Frontend/src/components/Card.tsx
@@ -1,15 +1,13 @@
-
 import { ShareIcon } from "../icons/ShareIcon";
 import { TrashIcon } from "../icons/TrashIcon";
 import { TwitterIcon } from "../icons/TwitterIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 
-interface CardProps {
-  title: string; 
+export interface CardProps {
+  title: string;
   type: "twitter" | "youtube";
-  link: string; 
+  link: string;
 }
-
 
 const iconMap = {
   twitter: TwitterIcon,
@@ -17,21 +15,23 @@ const iconMap = {
 };
 
 export function Card({ title, type, link }: CardProps) {
-  
   const Icon = iconMap[type];
 
   return (
     <div className="break-inside-avoid">
       <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border min-h-48 min-w-72">
         <div className="flex justify-between items-start">
-         
+          {/* Left: Platform Icon */}
           <div className="flex items-center text-gray-600 pt-1">
             <Icon />
           </div>
 
-          <div className="text-center font-semibold flex-grow mx-2">{title}</div>
+          {/* Center: Title */}
+          <div className="text-center font-semibold flex-grow mx-2 truncate">
+            {title}
+          </div>
 
-         
+          {/* Right: Share + Trash */}
           <div className="flex items-center text-gray-600 pt-1">
             <div className="pr-3">
               <a href={link} target="_blank" rel="noopener noreferrer">
@@ -39,25 +39,25 @@ export function Card({ title, type, link }: CardProps) {
               </a>
             </div>
             <div>
-              <TrashIcon/>
+              <TrashIcon />
             </div>
           </div>
         </div>
 
+        {/* Preview Section */}
         <div className="pt-4">
           {type === "youtube" && (
             <iframe
-              className="w-full aspect-video" 
-              src={link
-                .replace("watch", "embed")
-                .replace("?v=", "/")}
-              title="Youtube Video Player"
+              className="w-full aspect-video rounded-md"
+              src={link.replace("watch?v=", "embed/")}
+              title="YouTube Video Player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
             ></iframe>
           )}
+
           {type === "twitter" && (
             <blockquote className="twitter-tweet">
               <a href={link.replace("x.com", "twitter.com")}></a>
